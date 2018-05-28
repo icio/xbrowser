9f8c398c8bf4941d721a907e123c60291bacf407
Paul Scott <paul.scott@ravelin.com>
2018-05-28 20:56:07 +0100

    Tidy browser-conditional tests

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,30 +1,28 @@
 describe('ravelinjs', function() {
+    const cap = browser.desiredCapabilities;
+
     it('can be used with a script tag', function() {
         browser.url('/pages/scripttag/index.html');
         suite(browser);
     });
 
-    if (browser.desiredCapabilities.requireJSTestDisabled) {
-        it('cannot be used with requirejs', function() {});
-    } else {
-        it('can be used with requirejs', function() {
-            browser.url('/pages/amd/index.html');
-            suite(browser);
-        });
-    }
+    usuallyIt(!cap.requireJSTestDisabled, 'can be used with requirejs', function() {
+        browser.url('/pages/amd/index.html');
+        suite(browser);
+    });
 
-    if (browser.desiredCapabilities.webpackTestDisabled) {
-        it ('cannot be used with webpack', function() {});
-    } else {
-        it('can be used with webpack', function() {
-            browser.url('/pages/webpack/index.html');
-            suite(browser);
-        });
-    }
+    usuallyIt(!cap.webpackTestDisabled, 'can be used with webpack', function() {
+        browser.url('/pages/webpack/index.html');
+        suite(browser);
+    });
 });
 
 function suite(browser) {
     browser.setValue('#name', 'John');
     browser.click('#update');
     browser.getValue('#output').should.not.equal('');
+}
+
+function usuallyIt(itDoes) {
+    return (itDoes ? it : it.skip).apply(this, Array.prototype.slice.call(arguments, 1));
 }
