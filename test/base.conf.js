b00a3aff38dffbd51c158793f8f8d4c541376146
Paul Scott <paul.scott@ravelin.com>
2018-05-29 16:20:44 +0100

    Bump mocha timeout to 30 seconds

--- test/base.conf.js
+++ test/base.conf.js
@@ -131,7 +131,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 20000,
+        timeout: 30000,
     },
 
     // Test reporter for stdout.
